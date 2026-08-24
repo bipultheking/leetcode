4d8f11c7c5597fa1d265e7edfe449b0f1d6a77ae
bipultheking <bipulkumarsingh2580@gmail.com>
2026-08-24 20:27:45 +0530

    Time: 0 ms (100%), Space: 45.3 MB (25.21%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    
     int maxAbsoluteSum(vector<int>& nums) {
-        int bestcase = nums[0] ;
+        int bestcase = nums[0];
         int worstcase = nums[0];
         int maximum = abs(nums[0]);
-        
-        for(int i = 1;i<nums.size();i++){
-          int  sum1 = nums[i]+bestcase;
-          int  sum2 = nums[i]+worstcase;
-           if(nums[i] > sum1 && nums[i] > sum2) {
+
+        for(int i = 1; i < nums.size(); i++) {
+
+            int sum1 = nums[i] + bestcase;
+            int sum2 = nums[i] + worstcase;
+
+            if(nums[i] > sum1 && nums[i] > sum2) {
                 bestcase = nums[i];
             }
             else if(sum1 > sum2) {
@@ -18,24 +19,26 @@ public:
             else {
                 bestcase = sum2;
             }
-            if(nums[i]<sum1 && nums[i]<sum2){
+
+            if(nums[i] < sum1 && nums[i] < sum2) {
                 worstcase = nums[i];
-            }else if(sum1 < sum2){
+            }
+            else if(sum1 < sum2) {
                 worstcase = sum1;
-            }else{
+            }
+            else {
                 worstcase = sum2;
             }
-            if(maximum< bestcase){
+
+            if(maximum < bestcase) {
                 maximum = bestcase;
             }
-            if(maximum < (-worstcase)){
-                maximum = (-worstcase);
-            }
-           
 
-        }return maximum;
-        
+            if(maximum < abs(worstcase)) {
+                maximum = abs(worstcase);
+            }
+        }
+
+        return maximum;
     }
 };
-        
-    
