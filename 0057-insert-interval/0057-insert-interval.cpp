e8fdb1835073e838acd00162182208c82af144f2
bipultheking <bipulkumarsingh2580@gmail.com>
2026-09-07 19:51:06 +0530

    Time: 0 ms (100%), Space: 23.1 MB (6.01%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,34 +1,48 @@
 class Solution {
 public:
+
+    // try inserting new interval first then merge 
+
+
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>>res;
-
-        int i =0;
+        vector<vector<int>>array;
         int n = intervals.size();
+        bool insert = false;
+        for(int i =0;i<n;i++){
+            if(insert == false && intervals[i][0] >= newInterval[0]){
+                array.push_back(newInterval);
+                insert = true;
+            }
+            array.push_back(intervals[i]);
+        }
+        if(insert == false){
+            array.push_back(newInterval);
 
-        while(i<n && intervals[i][1] < newInterval[0]){
-            res.push_back(intervals[i]);
-            i++;
         }
 
-        int start1 = newInterval[0];
-        int end1 = newInterval[1];
+        // yaha tk insert hi kr rhe the ab ek array bn gya sorted insertion ke saath ab merge krte hai
 
-        while(i<n && intervals[i][0] <= end1){
-            int start2 = intervals[i][0];
-            int end2 = intervals[i][1];
+        vector<vector<int>>res;
+        int start1 = array[0][0];
+        int end1 = array[0][1];
 
-            start1 = min(start1,start2);
-            end1 = max(end1,end2);
+        for(int i =1; i<= n;i++){
+            int start2 = array[i][0];
+            int end2 = array[i][1];
 
-            i++;
+            if(end1 >= start2){
+                start1 = min(start1,start2);
+                end1 = max(end1,end2);
+                continue;
 
+            }
+            else{
+                res.push_back({start1,end1});
+                start1 = start2;
+                end1 = end2;
+
+            }
         }res.push_back({start1,end1});
-        while(i < n) {
-            res.push_back(intervals[i]);
-            i++;
-        }
-
         return res;
         
     }
