f72bfd17a863bdf200f5f10bff917a345fc0d377
bipultheking <bipulkumarsingh2580@gmail.com>
2026-09-16 22:28:36 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.42%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        unordered_map<char,int> hash;
+
+        for(int i = 0; i < s.size(); i++){
+            hash[s[i]]++;
+        }
+
+        for(int i = 0; i < t.size(); i++){
+            hash[t[i]]--;
+        }
+
+        for(auto x : hash){
+            if(x.second != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
